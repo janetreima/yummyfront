579390a411d074c329893dcc31864158230b5a58
jane <janetreima@gmail.com>
2023-12-04 11:37:39 +0200

    login modal wip

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,41 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <LoginModal ref="loginModalRef"/>
+  <div class="container text-center">
+    <div class="row">
+      <div class="col">
+        Column
+      </div>
+      <div class="col">
+        <nav>
+
+
+          <button @click="openLoginModal" type="button" class="btn btn-outline-dark">Logi sisse</button>
+          <router-link to="/">Home</router-link>
+          |
+          <router-link to="/about">About</router-link>
+        </nav>
+      </div>
+    </div>
+  </div>
   <router-view/>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+
+<script>
+
+import LoginModal from "@/components/LoginModal.vue";
+
+export default {
+  components: {LoginModal},
+  methods: {
+
+    openLoginModal() {
+      this.$refs.loginModalRef.$refs.modalRef.openModal()
+    }
+
+
+    ,
+  }
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
---
+++ src/components/LoginModal.vue
@@ -0,0 +1,35 @@
+<template>
+  <Modal ref="modalRef" button-name="Logi sisse">
+    <template #header>
+      Logi sisse
+    </template>
+    <template #body>
+      <div class="input-group mb-3">
+        <span class="input-group-text">kasutajanimi</span>
+        <input v-model="username" type="text" class="form-control">
+      </div>
+      <div class="input-group mb-3">
+        <span class="input-group-text">parool</span>
+        <input v-model="password" type="text" class="form-control">
+      </div>
+    </template>
+    <template #footer>
+      <button type="button" class="btn btn-outline-dark">Logi sisse</button>
+    </template>
+  </Modal>
+</template>
+
+<script>
+import Modal from "@/components/Modal.vue";
+
+export default {
+  name: 'LoginModal',
+  components: {Modal},
+  methods: {
+
+  },
+
+}
+
+</script>
+

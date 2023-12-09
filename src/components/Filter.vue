c7187cbe3aa30a9010fc919b88b266b6498a65f8
janetreima <janetreima@gmail.com>
2023-12-09 19:40:06 +0200

    Filter wip - dynamic checkboxes

--- src/components/Filter.vue
+++ src/components/Filter.vue
@@ -2,29 +2,24 @@
   <h4>
     Filtreeri
   </h4>
+  <h6>
+    Käik
+  </h6>
+  <div v-for="course in courses" :key="course.id"  class="form-check">
+    <input class="form-check-input" type="checkbox" value="">
+    <label class="form-check-label" for="flexCheckDefault">
+      {{ course.name }}
+    </label>
+  </div>
+  <h6>
+    Allergeenid
+  </h6>
   <div class="mt-3">
-    <h5>
-      Käik
-    </h5>
-    <div>
-      <div class="form-check">
-        <input class="form-check-input" type="checkbox" value="">
-        <label class="form-check-label" for="flexCheckDefault">
-          Supp
-        </label>
-      </div>
-      <div class="form-check">
-        <input class="form-check-input" type="checkbox" value="">
-        <label class="form-check-label" for="flexCheckChecked">
-          Praad
-        </label>
-      </div>
-      <div class="form-check">
-        <input class="form-check-input" type="checkbox" value="">
-        <label class="form-check-label" for="flexCheckChecked">
-          Magustoit
-        </label>
-      </div>
+    <div v-for="allergen in allergens" :key="allergen.allergenId"  class="form-check">
+      <input class="form-check-input" type="checkbox" value="">
+      <label class="form-check-label" for="flexCheckDefault">
+        {{ allergen.allergenName }}
+      </label>
     </div>
   </div>
   <div class="mt-3">
@@ -36,7 +31,45 @@
 export default {
   name: 'Filter',
   data() {
-    return {}
+    return {
+      allergens: [
+        {
+          allergenId: 0,
+          allergenName: '',
+          isAvailable: false
+        }
+      ],
+      courses: [
+        {
+          id: 0,
+          name: '',
+        }
+      ]
+    }
   },
+  methods: {
+    getAllAllergens() {
+      this.$http.get('/recipe/allergens')
+          .then(response => {
+            this.allergens = response.data;
+          })
+          .catch(error => {
+            this.errorResponse = error.response.data;
+          });
+    },
+    getAllCourses() {
+      this.$http.get('/recipe/courses')
+          .then(response => {
+            this.courses = response.data;
+          })
+          .catch(error => {
+            this.errorResponse = error.response.data;
+          });
+    },
+  },
+  mounted() {
+    this.getAllAllergens()
+    this.getAllCourses()
+  }
 }
 </script>
